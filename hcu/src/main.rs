f5d357d3b811b49e7fed373bea905086036e3006
Yorick de Wid <yorickdewid@users.noreply.github.com>
2022-09-30 13:11:12 +0200

    Upgrade HCU

---
+++ hcu/src/main.rs
@@ -0,0 +1,448 @@
+#![deny(unsafe_code)]
+#![deny(warnings)]
+#![no_main]
+#![no_std]
+
+use panic_halt as _;
+
+use stm32h7xx_hal::time::Hertz;
+
+const PKG_NAME: &str = env!("CARGO_PKG_NAME");
+const PKG_VERSION: &str = env!("CARGO_PKG_VERSION");
+const PKG_VERSION_MAJOR: &str = env!("CARGO_PKG_VERSION_MAJOR");
+const PKG_VERSION_MINOR: &str = env!("CARGO_PKG_VERSION_MINOR");
+const PKG_VERSION_PATCH: &str = env!("CARGO_PKG_VERSION_PATCH");
+
+/// High speed external clock frequency.
+const HSE: Hertz = Hertz::MHz(25);
+/// System clock frequency.
+const SYS_CLOCK: Hertz = Hertz::MHz(400);
+/// FDCAN peripheral clock.
+const FDCAN_CLOCK: Hertz = Hertz::MHz(32);
+/// USART peripheral clock.
+const USART_CLOCK: Hertz = Hertz::MHz(48);
+
+/// J1939 network address.
+const NET_ADDRESS: u8 = 0x4A;
+
+#[rtic::app(device = stm32h7xx_hal::stm32, peripherals = true, dispatchers = [USART1, USART2])]
+mod app {
+    use stm32h7xx_hal::gpio::{self};
+    use stm32h7xx_hal::prelude::*;
+    use stm32h7xx_hal::rcc;
+    use stm32h7xx_hal::watchdog::SystemWindowWatchdog;
+    use systick_monotonic::Systick;
+
+    /// 100 Hz / 10 ms granularity
+    #[monotonic(binds = SysTick, default = true)]
+    type Monotonic = Systick<100>;
+
+    #[shared]
+    struct SharedResources {
+        state: vecraft::state::System,
+        console: vecraft::console::Console,
+        canbus1: vecraft::can::Can<
+            stm32h7xx_hal::can::Can<stm32h7xx_hal::stm32::FDCAN1>,
+            fdcan::NormalOperationMode,
+        >,
+    }
+
+    #[local]
+    struct LocalResources {
+        led: vecraft::led::Led,
+        gc: vecraft::lsgc::GateControl,
+        watchdog: SystemWindowWatchdog,
+    }
+
+    #[init]
+    fn init(ctx: init::Context) -> (SharedResources, LocalResources, init::Monotonics) {
+        let mono = Systick::new(ctx.core.SYST, crate::SYS_CLOCK.to_Hz());
+
+        let pwr = ctx.device.PWR.constrain();
+        let pwrcfg = pwr.freeze();
+
+        // RCC
+        let rcc = ctx.device.RCC.constrain();
+        let mut ccdr = rcc
+            .use_hse(crate::HSE)
+            .sys_ck(crate::SYS_CLOCK)
+            .per_ck(32.MHz())
+            .pll1_strategy(rcc::PllConfigStrategy::Iterative)
+            .pll1_q_ck(crate::FDCAN_CLOCK)
+            .pll3_strategy(rcc::PllConfigStrategy::Iterative)
+            .pll3_p_ck(crate::SYS_CLOCK)
+            .pll3_q_ck(crate::USART_CLOCK)
+            .freeze(pwrcfg, &ctx.device.SYSCFG);
+
+        // Switch adc_ker_ck_input multiplexer to per_ck
+        ccdr.peripheral.kernel_adc_clk_mux(rcc::rec::AdcClkSel::PER);
+
+        ccdr.peripheral
+            .kernel_usart234578_clk_mux(rcc::rec::Usart234578ClkSel::PLL3_Q);
+
+        let mut watchdog = SystemWindowWatchdog::new(ctx.device.WWDG, &ccdr);
+
+        // GPIO
+        let gpioa = ctx.device.GPIOA.split(ccdr.peripheral.GPIOA);
+        let gpiob = ctx.device.GPIOB.split(ccdr.peripheral.GPIOB);
+        let gpioc = ctx.device.GPIOC.split(ccdr.peripheral.GPIOC);
+        let gpiod = ctx.device.GPIOD.split(ccdr.peripheral.GPIOD);
+        // let gpioe = ctx.device.GPIOE.split(ccdr.peripheral.GPIOE);
+
+        // UART
+        let console = vecraft::console::Console::new(
+            ctx.device
+                .USART2
+                .serial(
+                    (gpiod.pd5.into_alternate(), gpiod.pd6.into_alternate()),
+                    115200.bps(),
+                    ccdr.peripheral.USART2,
+                    &ccdr.clocks,
+                )
+                .unwrap(),
+        );
+
+        let fdcan_prec = ccdr
+            .peripheral
+            .FDCAN
+            .kernel_clk_mux(rcc::rec::FdcanClkSel::PLL1_Q);
+
+        let canbus1 = {
+            let rx = gpiod.pd0.into_alternate().speed(gpio::Speed::VeryHigh);
+            let tx = gpiod.pd1.into_alternate().speed(gpio::Speed::VeryHigh);
+
+            let pd3 = gpiod.pd3.into_push_pull_output();
+
+            vecraft::can::CanBuilder::new(ctx.device.FDCAN1.fdcan(tx, rx, fdcan_prec), pd3)
+                .set_bit_timing(vecraft::can::BITRATE_250K)
+                .set_net_address_filter(crate::NET_ADDRESS)
+                .build()
+        };
+
+        let pwr_swtich1 = gpiod.pd11.into_push_pull_output();
+        let pwr_swtich2 = gpioa.pa12.into_push_pull_output();
+
+        let (_, (pwm_high1, pwm_low1, pwm_high2, pwm_low2)) = ctx
+            .device
+            .TIM2
+            .pwm_advanced(
+                (
+                    gpioa.pa1.into_alternate(),
+                    gpioa.pa0.into_alternate(),
+                    gpioa.pa3.into_alternate(),
+                    gpioa.pa2.into_alternate(),
+                ),
+                ccdr.peripheral.TIM2,
+                &ccdr.clocks,
+            )
+            .frequency(100.Hz())
+            .period(32_767)
+            .finalize();
+
+        let (_, (pwm_high0, pwm_low0, pwm_high3, pwm_low3)) = ctx
+            .device
+            .TIM4
+            .pwm_advanced(
+                (
+                    gpiod.pd12.into_alternate(),
+                    gpiod.pd13.into_alternate(),
+                    gpiod.pd14.into_alternate(),
+                    gpiod.pd15.into_alternate(),
+                ),
+                ccdr.peripheral.TIM4,
+                &ccdr.clocks,
+            )
+            .frequency(100.Hz())
+            .period(32_767)
+            .finalize();
+
+        let (_, (pwm_high4, pwm_low4, pwm_high7, pwm_low7)) = ctx
+            .device
+            .TIM3
+            .pwm_advanced(
+                (
+                    gpioc.pc6.into_alternate(),
+                    gpioc.pc7.into_alternate(),
+                    gpioc.pc8.into_alternate(),
+                    gpioc.pc9.into_alternate(),
+                ),
+                ccdr.peripheral.TIM3,
+                &ccdr.clocks,
+            )
+            .frequency(100.Hz())
+            .period(32_767)
+            .finalize();
+
+        let (_, (pwm_high5, pwm_low5, pwm_high6, pwm_low6)) = ctx
+            .device
+            .TIM1
+            .pwm_advanced(
+                (
+                    gpioa.pa10.into_alternate(),
+                    gpioa.pa11.into_alternate(),
+                    gpioa.pa8.into_alternate(),
+                    gpioa.pa9.into_alternate(),
+                ),
+                ccdr.peripheral.TIM1,
+                &ccdr.clocks,
+            )
+            .frequency(100.Hz())
+            .period(32_767)
+            .finalize();
+
+        let gate0 = vecraft::lsgc::Gate::new(pwm_high0, pwm_low0);
+        let gate1 = vecraft::lsgc::Gate::new(pwm_high1, pwm_low1);
+        let gate2 = vecraft::lsgc::Gate::new(pwm_high2, pwm_low2);
+        let gate3 = vecraft::lsgc::Gate::new(pwm_high3, pwm_low3);
+        let gate4 = vecraft::lsgc::Gate::new(pwm_high4, pwm_low4);
+        let gate5 = vecraft::lsgc::Gate::new(pwm_high5, pwm_low5);
+        let gate6 = vecraft::lsgc::Gate::new(pwm_high6, pwm_low6);
+        let gate7 = vecraft::lsgc::Gate::new(pwm_high7, pwm_low7);
+
+        let mut gc = vecraft::lsgc::GateControl {
+            lockout0: pwr_swtich1,
+            lockout1: pwr_swtich2,
+            gate0,
+            gate1,
+            gate2,
+            gate3,
+            gate4,
+            gate5,
+            gate6,
+            gate7,
+        };
+        gc.lock();
+
+        motd::spawn().ok();
+        firmware_state::spawn().ok();
+        status_print::spawn().ok();
+
+        watchdog.start(75.millis());
+
+        (
+            SharedResources {
+                state: vecraft::state::System::with_boot(),
+                console,
+                canbus1,
+            },
+            LocalResources {
+                led: vecraft::led::Led::new(
+                    gpiob.pb13.into_push_pull_output(),
+                    gpiob.pb14.into_push_pull_output(),
+                    gpiob.pb12.into_push_pull_output(),
+                ),
+                gc,
+                watchdog,
+            },
+            init::Monotonics(mono),
+        )
+    }
+
+    #[task(shared = [state, console])]
+    fn motd(mut ctx: motd::Context) {
+        ctx.shared.console.lock(|console| {
+            use core::fmt::Write;
+
+            writeln!(console, "==========================").ok();
+            writeln!(console, r#"       //\\  ___"#).ok();
+            writeln!(console, r#"       Y  \\/_/=|"#).ok();
+            writeln!(console, r#"      _L  ((|_L_|"#).ok();
+            writeln!(console, r#"     (/\)(__(____)"#).ok();
+            writeln!(console).ok();
+            writeln!(console, "    Firmware : {}", crate::PKG_NAME).ok();
+            writeln!(console, "    Version  : {}", crate::PKG_VERSION).ok();
+            writeln!(console, "    Address  : 0x{:X?}", crate::NET_ADDRESS).ok();
+            writeln!(console).ok();
+            writeln!(console, "  Laixer Equipment B.V.").ok();
+            writeln!(console, "   Copyright (C) 2022").ok();
+            writeln!(console, "==========================").ok();
+        });
+
+        ctx.shared.state.lock(|state| state.boot_complete());
+    }
+
+    #[task(shared = [state, canbus1], local = [led, watchdog])]
+    fn firmware_state(mut ctx: firmware_state::Context) {
+        let is_bus_ok = ctx.shared.canbus1.lock(|canbus1| canbus1.is_bus_ok());
+
+        let state = ctx.shared.state.lock(|state| {
+            if is_bus_ok {
+                state.set_bus_error(false);
+            }
+
+            state.state()
+        });
+
+        ctx.local
+            .led
+            .set_color(&state.as_led(), &vecraft::led::LedState::Toggle);
+
+        if state.is_running() {
+            ctx.local.watchdog.feed();
+        }
+
+        firmware_state::spawn_after(50.millis().into()).unwrap();
+    }
+
+    #[task(shared = [state, canbus1, console], local = [])]
+    fn status_print(mut ctx: status_print::Context) {
+        let uptime = monotonics::now().duration_since_epoch();
+
+        let seconds = uptime.to_secs() % 60;
+        let minutes = uptime.to_minutes() % 60;
+        let hours = uptime.to_hours() % 24;
+
+        let state = ctx.shared.state.lock(|state| state.state());
+
+        ctx.shared.console.lock(|console| {
+            use core::fmt::Write;
+
+            writeln!(
+                console,
+                "[{:02}:{:02}:{:02}] State: {}",
+                hours, minutes, seconds, state
+            )
+            .ok();
+        });
+
+        let major: u8 = crate::PKG_VERSION_MAJOR.parse().unwrap();
+        let minor: u8 = crate::PKG_VERSION_MINOR.parse().unwrap();
+        let patch: u8 = crate::PKG_VERSION_PATCH.parse().unwrap();
+
+        let last_error = 0_u16;
+        let (last_error_high, last_error_low) = if last_error > 0 {
+            (last_error.to_le_bytes()[0], last_error.to_le_bytes()[1])
+        } else {
+            (0xff, 0xff)
+        };
+
+        let id = vecraft::j1939::IdBuilder::from_pgn(65_282)
+            .sa(crate::NET_ADDRESS)
+            .build();
+
+        let frame = vecraft::j1939::FrameBuilder::new(id)
+            .copy_from_slice(&[
+                0xff,
+                state.as_byte(),
+                major,
+                minor,
+                patch,
+                0xff,
+                last_error_high,
+                last_error_low,
+            ])
+            .build();
+
+        ctx.shared.canbus1.lock(|canbus1| canbus1.send(frame));
+
+        status_print::spawn_after(1.secs().into()).unwrap();
+    }
+
+    fn valve_value(value: i16) -> vecraft::lsgc::GateSide<u16, u16> {
+        match value {
+            v if v == 0 => vecraft::lsgc::GateSide::Hold,
+            v if v.is_positive() => vecraft::lsgc::GateSide::Up(value as u16),
+            _ => vecraft::lsgc::GateSide::Down((value + 1).abs() as u16),
+        }
+    }
+    fn valve_value32(value: i16) -> vecraft::lsgc::GateSide<u32, u32> {
+        match value {
+            v if v == 0 => vecraft::lsgc::GateSide::Hold,
+            v if v.is_positive() => vecraft::lsgc::GateSide::Up(value as u32),
+            _ => vecraft::lsgc::GateSide::Down((value + 1).abs() as u32),
+        }
+    }
+
+    #[task(binds = FDCAN1_IT0, priority = 2, shared = [canbus1, state, console], local = [gc])]
+    fn can1_event(mut ctx: can1_event::Context) {
+        let is_bus_error = ctx.shared.canbus1.lock(|canbus1| canbus1.is_bus_error());
+
+        if is_bus_error {
+            ctx.shared.state.lock(|state| state.set_bus_error(true));
+        }
+
+        if let Some(frame) = ctx.shared.canbus1.lock(|canbus1| canbus1.recv()) {
+            match frame.id().pgn() {
+                45_312 => {
+                    if frame.pdu()[0] == b'Z' && frame.pdu()[1] == b'C' {
+                        if frame.pdu()[2] & 0b00000001 == 1 {
+                            ctx.shared.state.lock(|state| state.set_ident(true));
+                        } else if frame.pdu()[2] & 0b00000001 == 0 {
+                            ctx.shared.state.lock(|state| state.set_ident(false));
+                        }
+
+                        if frame.pdu()[3] == 0x69 {
+                            vecraft::sys_reset();
+                        }
+                    }
+                }
+                45_824 => {
+                    if frame.pdu()[0] == b'Z' && frame.pdu()[1] == b'C' {
+                        if frame.pdu()[3] & 0b11 == 0 {
+                            ctx.shared.console.lock(|console| {
+                                use core::fmt::Write;
+
+                                writeln!(console, "Motion locked").ok();
+                            });
+
+                            ctx.local.gc.lock();
+                        } else if frame.pdu()[3] & 0b11 == 1 {
+                            ctx.shared.console.lock(|console| {
+                                use core::fmt::Write;
+
+                                writeln!(console, "Motion unlocked").ok();
+                            });
+
+                            ctx.local.gc.unlock();
+                        }
+                    }
+                }
+                40_960 => {
+                    if frame.pdu()[0..2] != [0xff, 0xff] {
+                        let gate_value = i16::from_le_bytes(frame.pdu()[0..2].try_into().unwrap());
+
+                        ctx.local.gc.gate0.set_value(valve_value(gate_value));
+                    }
+                    if frame.pdu()[2..4] != [0xff, 0xff] {
+                        let gate_value = i16::from_le_bytes(frame.pdu()[2..4].try_into().unwrap());
+
+                        ctx.local.gc.gate1.set_value(valve_value32(gate_value));
+                    }
+                    if frame.pdu()[4..6] != [0xff, 0xff] {
+                        let gate_value = i16::from_le_bytes(frame.pdu()[4..6].try_into().unwrap());
+
+                        ctx.local.gc.gate2.set_value(valve_value32(gate_value));
+                    }
+                    if frame.pdu()[6..8] != [0xff, 0xff] {
+                        let gate_value = i16::from_le_bytes(frame.pdu()[6..8].try_into().unwrap());
+
+                        ctx.local.gc.gate3.set_value(valve_value(gate_value));
+                    }
+                }
+                41_216 => {
+                    if frame.pdu()[0..2] != [0xff, 0xff] {
+                        let gate_value = i16::from_le_bytes(frame.pdu()[0..2].try_into().unwrap());
+
+                        ctx.local.gc.gate4.set_value(valve_value(gate_value));
+                    }
+                    if frame.pdu()[2..4] != [0xff, 0xff] {
+                        let gate_value = i16::from_le_bytes(frame.pdu()[2..4].try_into().unwrap());
+
+                        ctx.local.gc.gate5.set_value(valve_value(gate_value));
+                    }
+                    if frame.pdu()[4..6] != [0xff, 0xff] {
+                        let gate_value = i16::from_le_bytes(frame.pdu()[4..6].try_into().unwrap());
+
+                        ctx.local.gc.gate6.set_value(valve_value(gate_value));
+                    }
+                    if frame.pdu()[6..8] != [0xff, 0xff] {
+                        let gate_value = i16::from_le_bytes(frame.pdu()[6..8].try_into().unwrap());
+
+                        ctx.local.gc.gate7.set_value(valve_value(gate_value));
+                    }
+                }
+                _ => {}
+            }
+        }
+    }
+}
