927cb8fe2811dbd32cf8a4888bcee34dbeaaf4f8
Yorick de Wid <yorickdewid@users.noreply.github.com>
2024-04-12 17:13:58 +0200

    Refactor VecraftConfig struct and add new constructor and to_bytes method

--- vecraft/src/config.rs
+++ vecraft/src/config.rs
@@ -1,20 +1,24 @@
 // EEPROM
 // Header + version + firmware mode: [0x4C, 0x58, 0x52, 0x1, 0x16, 0xFF, 0xFF, 0xF]
 // Serial number: [0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8]
-
+//
 // Uart
 // Selection : [2]
 // Baudrate  : [0, 194, 1, 0]
-
+//
 // Canbus 1
 // bit timing  : [144, 208, 3, 0]
 // Termination : [0]
-
+//
 // J1939
 // Address : [0x4A]
 // Name    : [0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8]
 
 pub const VECRAFT_CONFIG_PAGE: usize = 1;
+
+const VECRAFT_CONFIG_SIZE: usize = 64;
+const VECRAFT_CONFIG_HEADER: [u8; 3] = *b"LXR";
+const VECRAFT_CONFIG_VERSION: u8 = 0x1;
 
 #[derive(Copy, Clone, Debug)]
 pub struct VecraftConfig {
@@ -29,6 +33,19 @@ pub struct VecraftConfig {
 }
 
 impl VecraftConfig {
+    pub fn new(ecu_mode: u8, serial_number: [u8; 8], j1939_name: [u8; 8]) -> Self {
+        Self {
+            ecu_mode,
+            serial_number,
+            uart_selected: 0,
+            uart_baudrate: 0,
+            canbus1_bitrate: 0,
+            canbus1_termination: false,
+            j1939_address: 0,
+            j1939_name,
+        }
+    }
+
     pub fn ecu_mode(&self) -> u8 {
         self.ecu_mode
     }
@@ -47,6 +64,23 @@ impl VecraftConfig {
     pub fn j1939_name(&self) -> j1939::Name {
         j1939::Name::from_bytes(self.j1939_name)
     }
+
+    pub fn to_bytes(&self) -> [u8; VECRAFT_CONFIG_SIZE] {
+        let mut bytes = [0xFF; VECRAFT_CONFIG_SIZE];
+
+        bytes[0..3].copy_from_slice(&VECRAFT_CONFIG_HEADER);
+        bytes[3] = VECRAFT_CONFIG_VERSION;
+        bytes[4] = self.ecu_mode;
+        bytes[8..16].copy_from_slice(&self.serial_number);
+        bytes[32] = self.uart_selected;
+        bytes[33..37].copy_from_slice(&self.uart_baudrate.to_le_bytes());
+        bytes[40..44].copy_from_slice(&self.canbus1_bitrate.to_le_bytes());
+        bytes[44] = self.canbus1_termination as u8;
+        bytes[48] = self.j1939_address;
+        bytes[49..57].copy_from_slice(&self.j1939_name);
+
+        bytes
+    }
 }
 
 // TODO: Replace with an actual error type
@@ -54,13 +88,14 @@ impl TryFrom<&[u8]> for VecraftConfig {
     type Error = u8;
 
     fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
-        if &value[0..3] != b"LXR" {
+        if value[0..3] != VECRAFT_CONFIG_HEADER {
             return Err(1);
         }
-        if value[3] != 0x1 {
+        if value[3] != VECRAFT_CONFIG_VERSION {
             return Err(2);
         }
 
+        // TODO: Ignore values that fail to convert
         Ok(Self {
             ecu_mode: value[4],
             serial_number: value[8..16].try_into().unwrap(),
@@ -73,3 +108,34 @@ impl TryFrom<&[u8]> for VecraftConfig {
         })
     }
 }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn test_config_roundtrip() {
+//         let config = VecraftConfig {
+//             ecu_mode: 0x1,
+//             serial_number: [0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8],
+//             uart_selected: 2,
+//             uart_baudrate: 115200,
+//             canbus1_bitrate: 500_000,
+//             canbus1_termination: false,
+//             j1939_address: 0x4A,
+//             j1939_name: [0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8],
+//         };
+
+//         let bytes = config.to_bytes();
+//         let roundtrip = VecraftConfig::try_from(&bytes[..]).unwrap();
+
+//         assert_eq!(config.ecu_mode, roundtrip.ecu_mode);
+//         assert_eq!(config.serial_number, roundtrip.serial_number);
+//         assert_eq!(config.uart_selected, roundtrip.uart_selected);
+//         assert_eq!(config.uart_baudrate, roundtrip.uart_baudrate);
+//         assert_eq!(config.canbus1_bitrate, roundtrip.canbus1_bitrate);
+//         assert_eq!(config.canbus1_termination, roundtrip.canbus1_termination);
+//         assert_eq!(config.j1939_address, roundtrip.j1939_address);
+//         assert_eq!(config.j1939_name, roundtrip.j1939_name);
+//     }
+// }
