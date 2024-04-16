ad59fe1977cfb63af7eb5039237eda34ce9c6f2c
Yorick de Wid <yorickdewid@users.noreply.github.com>
2024-04-16 23:40:35 +0200

    Refactor EcuApplication enum and conversion implementations

--- vecraft/src/lib.rs
+++ vecraft/src/lib.rs
@@ -20,6 +20,10 @@ pub mod usb_frame;
 pub use config::{ConfigError, VecraftConfig, VECRAFT_CONFIG_PAGE};
 pub use led::{LedState, RGBLed};
 
+/// Represents the different applications of an ECU.
+///
+/// The ECU application is used to determine firmware behavior and message handling. A single ECU
+/// can have multiple applications, but only one application can be active at a time.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum EcuApplication {
     PumpControl,
@@ -27,9 +31,19 @@ pub enum EcuApplication {
     HydraulicControl,
 }
 
+/// Converts a u8 value into an `EcuApplication` enum variant.
 impl TryFrom<u8> for EcuApplication {
     type Error = ();
 
+    /// Converts the given `u8` value into its corresponding `EcuApplication` variant.
+    ///
+    /// # Arguments
+    ///
+    /// * `value` - The `u8` value to convert.
+    ///
+    /// # Returns
+    ///
+    /// The `EcuApplication` variant corresponding to the given `u8` value.
     fn try_from(value: u8) -> Result<Self, Self::Error> {
         match value {
             0x10 => Ok(Self::PumpControl),
@@ -40,7 +54,17 @@ impl TryFrom<u8> for EcuApplication {
     }
 }
 
+/// Converts an `EcuApplication` enum variant into a `u8` value.
 impl From<EcuApplication> for u8 {
+    /// Converts the given `EcuApplication` variant into its corresponding `u8` value.
+    ///
+    /// # Arguments
+    ///
+    /// * `value` - The `EcuApplication` variant to convert.
+    ///
+    /// # Returns
+    ///
+    /// The `u8` value corresponding to the given `EcuApplication` variant.
     fn from(value: EcuApplication) -> Self {
         match value {
             EcuApplication::PumpControl => 0x10,
